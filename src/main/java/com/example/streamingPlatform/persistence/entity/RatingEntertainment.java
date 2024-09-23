804af04b2fb6e619a636f0262547e9e9282bd0fe
Alex Nanclares <alex.nanclares.7@gmail.com>
2024-09-22 19:26:50 -0500

    Se cambia mensaje a español para manejar un estandar en español con los mensajes para el cliente

---
+++ src/main/java/com/example/streamingPlatform/persistence/entity/RatingEntertainment.java
@@ -0,0 +1,9 @@
+package com.example.streamingPlatform.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "")
+public class rating_entertainment {
+}
