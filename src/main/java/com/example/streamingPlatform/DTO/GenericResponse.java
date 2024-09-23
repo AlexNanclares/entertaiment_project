c3d409d56bff94a9c04842aca2f933181d17286f
Alex Nanclares <alex.nanclares.7@gmail.com>
2024-09-22 19:32:28 -0500

    Se agrega response generico para responder en excepciones y validaciones

---
+++ src/main/java/com/example/streamingPlatform/DTO/GenericResponse.java
@@ -0,0 +1,16 @@
+package com.example.streamingPlatform.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenericResponse {
+    private Integer code;
+    private String message;
+    private Object result;
+}
